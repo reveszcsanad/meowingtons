66e8c60e9b7fd2fe5ddb2b52af519b3701997669
Révész Csanád <138862790+reveszcsanad@users.noreply.github.com>
2023-11-14 10:42:56 +0100

    unsubscribe button never goes out

--- dynamic.js
+++ dynamic.js
@@ -1,10 +1,22 @@
 function jumpToRandomPosition() {
     let button = document.getElementById('unsubscribe-btn');
 
-    let randomX = Math.random() * window.innerWidth;
-    let randomY = Math.random() * window.innerHeight;
+    var randomX = Math.random() * (window.innerWidth - button.offsetWidth);
+    var randomY = Math.random() * (window.innerHeight - button.offsetHeight);
 
     button.style.position = 'absolute';
     button.style.left = randomX + 'px';
     button.style.top = randomY + 'px';
+}
+
+function checkButtonPosition() {
+    var button = document.getElementById('unsubscribe-btn');
+
+    if (button.offsetTop + button.offsetHeight > window.innerHeight) {
+        button.style.top = window.innerHeight - button.offsetHeight + 'px';
+    }
+
+    if (button.offsetLeft + button.offsetWidth > window.innerWidth) {
+        button.style.left = window.innerWidth - button.offsetWidth + 'px';
+    }
 }
